eca317dbf6c4e077e0d7bac128f2cbca1a74ca3c
asieveking <asievekingg@hotmail.com>
2022-05-15 21:38:29 -0400

    cambio para llamar a los modulos

--- Analyst/create_txt_from_db.py
+++ Analyst/create_txt_from_db.py
@@ -2,7 +2,8 @@ import sys
 import os
 
 sys.path.append(os.path.join(os.path.dirname(sys.path[0])))
-import sample.sql_connection as sql_connection
+
+from sample import sql_connection
 
 import pandas as pd
 
